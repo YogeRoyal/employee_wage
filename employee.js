ea57f8aefca5732bad4787364732bec1e157db7a
unknown <kaketiyogesh2110.com>
2023-12-07 14:56:43 +0530

    UC2:daily wage of employee calculated

--- employee.js
+++ employee.js
@@ -1,8 +1,8 @@
-
+// uc2 daily wage of employee
 console.log("Welcome to Employee Wage Computation");
 
 employeDetails = [
-    { Eid: 1001, name: "Adarsh" },
+    { Eid: 1001, name: "Murugan" },
     { Eid: 1001, name: "krishna" },
     { Eid: 1001, name: "Dhanush" },
 ]
@@ -10,21 +10,24 @@ employeDetails = [
 let details = []
 let arr = {}
 function showDetails() {
-    console.log(
-        `+-------+--------------------+-----------+
-|   Id  |        Name        | Attendence |
-+-------+--------------------+------------+`);
+    console.log(`+-------+-------------------------+-----------+-------------+
+|   Id  |        Name        |  Attendence   | DailyWage($)  |
++-------+--------------------+----------------+------------+`);
     det = employeDetails.forEach(element => {
         const Eid = element.Eid;
         const name = element.name;
         let attendence = Math.round(Math.random());
+        let workedHours = attendence * 8;
+        let dailyWage=workedHours*20;
         arr = {
             Eid: Eid,
             name: name,
             attendence: attendence,
+            dailyWage:dailyWage
         }
-        console.log(`|  ${Eid} |        ${name}       |  ${attendence}  |`);
+        console.log(`|  ${Eid} |        ${name}     |    ${attendence=(attendence===0?"absent":"present")}    |     ${dailyWage}     |`);
     });
-    console.log("+------+-------------+---------+");
+    console.log("+-------+---------------------+------------+--------------+");
 }
+
 showDetails();
