2dc01ce4e00fdc326fa0d6d1f5641113d7aa8484
unknown <kaketiyogesh2110.com>
2023-12-07 13:33:01 +0530

    UC1 completed

--- employee.js
+++ employee.js
@@ -1,1 +1,29 @@
 console.log("Welcome to Employee Wage Computation");
+
+employeDetails = [
+    { Eid: 1001, name: "Adarsh" },
+    { Eid: 1001, name: "krishna" },
+    { Eid: 1001, name: "Dhanush" },
+]
+
+let details = []
+let arr = {}
+function showDetails() {
+    console.log(
+        `+-------+--------------------+-----------+
+|   Id  |        Name        | Attendence |
++-------+--------------------+------------+`);
+    det = employeDetails.forEach(element => {
+        const Eid = element.Eid;
+        const name = element.name;
+        let attendence = Math.round(Math.random());
+        arr = {
+            Eid: Eid,
+            name: name,
+            attendence: attendence,
+        }
+        console.log(`|  ${Eid} |        ${name}       |  ${attendence}  |`);
+    });
+    console.log("+------+-------------+---------+");
+}
+showDetails();
