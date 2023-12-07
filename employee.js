d3823ab3ec7e988c8aa1d87aede9ee6a401c42dd
unknown <kaketiyogesh2110.com>
2023-12-07 15:27:37 +0530

    UC3:added employee type and calculated their wages

--- employee.js
+++ employee.js
@@ -1,4 +1,4 @@
-// uc2 daily wage of employee
+// uc3 adding employee type and their wages
 console.log("Welcome to Employee Wage Computation");
 
 employeDetails = [
@@ -10,24 +10,29 @@ employeDetails = [
 let details = []
 let arr = {}
 function showDetails() {
-    console.log(`+-------+-------------------------+-----------+-------------+
-|   Id  |        Name        |  Attendence   | DailyWage($)  |
-+-------+--------------------+----------------+------------+`);
+    console.log(`+-------+--------------------+----------------+---------------+-----------------+`)
+    console.log(`|   Id  |        Name        |  Attendence    | Employee_Type |   DailyWage($)  |`)
+    console.log(`+-------+--------------------+----------------+---------------+-----------------+`);
     det = employeDetails.forEach(element => {
         const Eid = element.Eid;
         const name = element.name;
         let attendence = Math.round(Math.random());
         let workedHours = attendence * 8;
-        let dailyWage=workedHours*20;
+        let dailyWage = workedHours * 20;
+        let arr = ["part time", "full time"]
+        let index = Math.floor(Math.random() * arr.length)
+
+        // console.log(arr[index]);
+        let employee_type = (arr[index])
         arr = {
             Eid: Eid,
             name: name,
             attendence: attendence,
-            dailyWage:dailyWage
+            dailyWage: dailyWage
         }
-        console.log(`|  ${Eid} |        ${name}     |    ${attendence=(attendence===0?"absent":"present")}    |     ${dailyWage}     |`);
+        console.log(`|  ${Eid} |        ${name}     |    ${attendence = (attendence === 0 ? "absent" : "present")}      |   ${employee_type}  |         ${dailyWage = (employee_type === "part time" ? dailyWage / 2 : dailyWage)}       |`);
     });
-    console.log("+-------+---------------------+------------+--------------+");
+    console.log("+-------+--------------------+---------------+---------------+--------------+");
 }
 
-showDetails();
+showDetails()
