a4927dc01b205411a793597b12576c2baa756968
unknown <kaketiyogesh2110.com>
2023-12-08 12:02:29 +0530

    UC5:added monthly wage of employee

--- employee.js
+++ employee.js
@@ -9,20 +9,21 @@ employeDetails = [
 
 let arr = {}
 function showDetails() {
-    console.log(`+-------+--------------------+----------------+---------------+-----------------+----------------+`)
-    console.log(`|   Id  |        Name        |  Attendence    | Employee_Type |   DailyWage($)  |   Monthly Wage |`)
-    console.log(`+-------+--------------------+----------------+---------------+-----------------+----------------+`);
+    console.log(`+----+----------+-----------+---------------+---------------+--------------------+---------------+`)
+    console.log(`| Id |   Name   | Attendence| Employee_Type | DailyWage($)  | No.Of working Days |  Monthly Wage |`)
+    console.log(`+----+----------+-----------+---------------+---------------+--------------------+---------------+`);
     details = employeDetails.forEach(element => {
         const Eid = element.Eid;
         const name = element.name;
         let attendence = Math.round(Math.random());
         let workedHours = attendence * 8;
         let dailyWage = workedHours * 20;
-        let arr = ["part time", "full time"]
-        let index = Math.floor(Math.random() * arr.length);
-        let employee_type = (arr[index]);
-        let monthlyWage = monthly_Wage(attendence, dailyWage);
-        function monthly_Wage(attendence, dailyWage) {
+        // let arr = ["part time", "full time"]
+        // let index = Math.floor(Math.random() * arr.length);
+        let employee_type = Math.round(Math.random());
+        let {count,wage} = monthly_Wage(attendence, dailyWage);
+        // console.log(count);
+        function monthly_Wage(attendence, dailyWage,employee_type) {
             let count = 0
             if (attendence === 1) {
                 for (let i = 1; i <= 30; i++) {
@@ -38,18 +39,19 @@ function showDetails() {
             if (count > 20) {
                 count = 20
             }
+            return {count:count,wage:count*dailyWage}
             // dailyWage = (employee_type === "part time" ? dailyWage / 2 : dailyWage)
-            return count * dailyWage
+            // return count * dailyWage
         }
         arr = {
             Eid: Eid,
             name: name,
             attendence: attendence,
             dailyWage: dailyWage,
-            monthlyWage: monthlyWage,
+            employee_type:employee_type,
 
         }
-        console.log(`|  ${Eid} |        ${name}     |    ${padString(attendence = (attendence === 0 ? "absent" : "present"), 6)}      |   ${employee_type}  |         ${padNum(dailyWage = (employee_type === "part time" ? dailyWage / 2 : dailyWage), 3)}       |  ${padNum(monthlyWage, 8)}    |`);
+        console.log(`|${padNum(Eid,3)}| ${padString(name,8)} |  ${padString(attendence = (attendence === 0 ? "absent" : "present"), 6)}  |   ${employee_type=(employee_type===0?"part time":"full time")}   |      ${padNum(dailyWage = (employee_type === "part time" ? dailyWage / 2 : dailyWage), 3)}       |        ${padNum(count, 8)}    |       ${wage=(employee_type==="full time"?wage:wage/2)}     |`);
     });
     console.log("+-------+--------------------+---------------+---------------+----------------+------------------+");
 
